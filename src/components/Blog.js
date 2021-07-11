e04c7b94642e2c4a9b2350269b3f5325719e9ed5
Heikki Koivula <heikki.koivula@gmail.com>
2021-07-11 13:51:38 +0300

    5.7 complete
    make button to show or hide full information of a blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [viewFullBlog, setViewFullBlog] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {viewFullBlog ? (
+        <div>
+          {blog.title} by {blog.author}{' '}
+          <button onClick={() => setViewFullBlog(!viewFullBlog)}>
+            hide
+          </button>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.user.name}
+        </div>
+      ) : (
+        <div>
+          {blog.title} by {blog.author}{' '}
+          <button onClick={() => setViewFullBlog(!viewFullBlog)}>
+            show
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
