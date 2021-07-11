48376d997ffc1b31f5cbae0dad4891314574b4b6
Heikki Koivula <heikki.koivula@gmail.com>
2021-07-11 13:25:11 +0300

    5.6 complete
    move state relating to creating a blog from App.js to BlogForm.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newTitle, setNewTitle] = useState('')
-  const [newAuthor, setNewAuthor] = useState('')
-  const [newUrl, setNewUrl] = useState('')
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -36,18 +33,6 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
-
-  const handleTitleChange = (event) => {
-    setNewTitle(event.target.value)
-  }
-
-  const handleAuthorChange = (event) => {
-    setNewAuthor(event.target.value)
-  }
-
-  const handleUrlChange = (event) => {
-    setNewUrl(event.target.value)
-  }
 
   const handleNameChange = (event) => {
     setUsername(event.target.value)
@@ -97,22 +82,12 @@ const App = () => {
     }, 5000)
   }
 
-  const addBlog = async (event) => {
-    event.preventDefault()
+  const addBlog = async (blogObject) => {
     blogFormRef.current.toggleVisibility()
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl
-    }
 
     try {
       const returnedBlog = await blogService.create(blogObject)
       setBlogs(blogs.concat(returnedBlog))
-
-      setNewTitle('')
-      setNewAuthor('')
-      setNewUrl('')
       setAttentionMessage(
         `a new blog "${returnedBlog.title}" by ${returnedBlog.author} added`
       )
@@ -130,15 +105,7 @@ const App = () => {
 
   const blogForm = () => (
     <Togglable buttonLabel="create new blog" ref={blogFormRef}>
-      <BlogForm
-        addBlog={addBlog}
-        newTitle={newTitle}
-        titleHandler={handleTitleChange}
-        newAuthor={newAuthor}
-        authorHandler={handleAuthorChange}
-        newUrl={newUrl}
-        urlHandler={handleUrlChange}
-      />
+      <BlogForm createBlog={addBlog} />
     </Togglable>
   )
 
@@ -170,7 +137,6 @@ const App = () => {
             {user.name} logged in{' '}
             <button onClick={handleLogout}>logout</button>
           </p>
-          <h2>create new</h2>
           {blogForm()}
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,27 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BlogForm = ({
-  addBlog,
-  newTitle,
-  titleHandler,
-  newAuthor,
-  authorHandler,
-  newUrl,
-  urlHandler
-}) => {
+const BlogForm = ({ createBlog }) => {
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    })
+
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
+
   return (
-    <form onSubmit={addBlog}>
-      <div>
-        title: <input value={newTitle} onChange={titleHandler} />
-      </div>
-      <div>
-        author: <input value={newAuthor} onChange={authorHandler} />
-      </div>
-      <div>
-        url: <input value={newUrl} onChange={urlHandler} />
-      </div>
-      <button type="submit">create</button>
-    </form>
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:{' '}
+          <input value={newTitle} onChange={handleTitleChange} />
+        </div>
+        <div>
+          author:{' '}
+          <input value={newAuthor} onChange={handleAuthorChange} />
+        </div>
+        <div>
+          url: <input value={newUrl} onChange={handleUrlChange} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
   )
 }
 
