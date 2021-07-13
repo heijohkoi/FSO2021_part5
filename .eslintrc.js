35e33038922caea1600521fa1ac1e67c42bfb059
Heikki Koivula <heikki.koivula@gmail.com>
2021-07-13 12:44:37 +0300

    5.12 complete
    change eslint configuration and add displayname to Togglable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   env: {
-    node: true,
-    commonjs: true,
-    es2021: true,
-    jest: true
+    browser: true,
+    es6: true,
+    'jest/globals': true
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
-    ecmaVersion: 12
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
+  plugins: ['react', 'jest'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
@@ -18,6 +22,12 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 0
+    'no-console': 0,
+    'react/prop-types': 0
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
