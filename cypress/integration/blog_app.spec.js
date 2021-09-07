4d8f5eaa5504ef0079f6d27d3daa7b7609b57b04
Heikki Koivula <heikki.koivula@gmail.com>
2021-09-07 11:29:48 +0300

    complete 5.18
    add test to login

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -13,5 +19,40 @@ describe('Blog app', function () {
     cy.get('#username')
     cy.get('#password')
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.contains('login').click()
+
+      cy.get('.attentionContent').should(
+        'contain',
+        'logged in as Matti Luukkainen'
+      )
+      cy.get('.attentionContent').should(
+        'have.css',
+        'color',
+        'rgb(0, 128, 0)'
+      )
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('random')
+      cy.get('#password').type('wrong')
+      cy.contains('login').click()
+
+      cy.get('.alertContent').should('contain', 'Wrong credentials')
+      cy.get('.alertContent').should(
+        'have.css',
+        'color',
+        'rgb(255, 0, 0)'
+      )
+      cy.get('#username')
+      cy.get('#password')
+      cy.contains('login')
+    })
   })
 })
